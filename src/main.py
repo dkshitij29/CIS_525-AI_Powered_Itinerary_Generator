325c4f95d547fa067567528ebfdcb3f8dc7426dc
User D <dkshitij29@gmail.com>
2025-10-16 18:17:31 -0400

    added the cors package

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import google.generativeai as genai
 from google import genai
